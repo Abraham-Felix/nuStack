0aca9a779694123ee9cf87ba8a6235113f4e7e97
abraham-felix <apesyntax@gmail.com>
2021-10-10 15:27:06 -0700

    articles page route link

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #defining root view
   root "home#index"
+  #defining link routes
   get "/home", to: "home#index"
+  get "/articles", to: "articles#index"
+
   #this is a resourcefull method for routing all CRUD conventional operations
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
